66aaaa9faf5aceabe2f5f684f4063aa9e21f40d6
Lionel Mason <capitalcookhouse@gmail.com>
2023-11-06 00:35:54 -0500

    updates Home componenet to show date

--- client/src/components/auth/Home.jsx
+++ client/src/components/auth/Home.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HomePage from "../images/HomePage.jpg"
 
+function getDate() {
+  const today = new Date();
+  const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  const month = monthNames[today.getMonth()];
+  const year = today.getFullYear();
+  const date = today.getDate();
+  const hours = today.getHours() % 12 || 12;
+  const minutes = today.getMinutes();
+  const ampm = today.getHours() >= 12 ? 'pm' : 'am';
+  return `Bronx, NY | ${month}, ${date}, ${year} | ${hours}:${minutes.toString().padStart(2, '0')}${ampm} EST`;
+}
+
 const Home = () => {
+  const [currentDate, setCurrentDate] = useState(getDate());
+
+  const dateStyle = {
+    fontSize: '16px', // Adjust the font size as needed
+  };
+
   return (
     <div className="containter-fluid">
       <div className="has-bg-img">
         <h2 className="text-center">DEMEURE</h2>
-        <h4 className="text-center">It's easy to set background image with Torus Kit</h4>
+        <h4 className="text-center" style={dateStyle}>{currentDate}</h4>
         <div>
           <div style={{
             height: "100vh",
@@ -14,13 +35,11 @@ const Home = () => {
             backgroundImage: `url(${HomePage})`,
             backgroundPosition: "center",
           }} className="bg-img mx-auto"></div>
-
         </div>
-        {/* <img style={{height: "100vh"}} className="bg-img mx-auto img-fluid" src={HomePage}alt={HomePage}/> */}
       </div>
-
     </div>
   )
 }
 
 export default Home;
+
